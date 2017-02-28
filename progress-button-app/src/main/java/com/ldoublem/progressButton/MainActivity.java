259909990a74ff62ec9e969bbc756dedd38e76a9
[201216323] <[1060140613@qq.com]>
2017-02-28 15:49:12 +0800

    Initial commit

---
+++ progress-button-app/src/main/java/com/ldoublem/progressButton/MainActivity.java
@@ -0,0 +1,62 @@
+package com.ldoublem.progressButton;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ldoublem.progressButton.view.ProgressButton;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ProgressButton pb_button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        pb_button = (ProgressButton) findViewById(R.id.pb_btn);
+        pb_button.setBgColor(Color.RED);
+        pb_button.setTextColor(Color.WHITE);
+        pb_button.setProColor(Color.WHITE);
+        pb_button.setButtonText("签到");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.pb_btn:
+                pb_button.startAnim();
+                Message m = mHandler.obtainMessage();
+                mHandler.sendMessageDelayed(m, 1500);
+                break;
+            default:
+                break;
+        }
+    }
+
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+
+            pb_button.stopAnim(new ProgressButton.OnStopAnim() {
+                @Override
+                public void Stop() {
+//                    Intent i=new Intent();
+//                    i.setClass(MainActivity.this,SecondActivity.class);
+//                    startActivity(i);
+                    pb_button.setBgColor(Color.TRANSPARENT);
+                    pb_button.setTextColor(Color.RED);
+                    pb_button.setButtonText("签到成功 √");
+                }
+            });
+
+        }
+    };
+
+
+}
